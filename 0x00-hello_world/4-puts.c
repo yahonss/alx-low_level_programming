6b08403e9bcaf1154a1402e279d16e980360392f
yahonss <safahmimda@gmail.com>
2023-04-23 13:03:13 -0700

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - a C-Program that prints "Programming is like building a multilingual puzzle"
+ * Return:the value 0
+ */
+int main(){
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+return 0;
+}
