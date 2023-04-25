68af3b80b2c41d1d2831edabd3e4bce967ed2853
yahonss <safahmimda@gmail.com>
2023-04-25 13:17:40 -0700

    new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_alphabet- print the alphabet
+ *is written by me
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
+}
