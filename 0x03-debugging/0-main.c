a1711b20248af21d1eabe0e04971d0608ba4f057
yahonss <safahmimda@gmail.com>
2023-04-27 12:23:35 -0700

    new commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * main - test for a postitive_or_negative function 
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
-	i=0;
+	i = 0;
+
 	positive_or_negative(i);
 return (0);
 
