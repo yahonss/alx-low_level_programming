a6b072c05086ec1e79e6503d1a271bcdf2bba76e
yahonss <safahmimda@gmail.com>
2023-04-28 14:00:24 -0700

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,16 +16,16 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-	if (i = 1; i <= 100; i++)
-	printf("FizzBuzz");
-	else if (i % 3 == 0)
-	printf("Fizz");
-	else if (i % 5 == 0)
-	printf("Buzz");
-	else
-	printf("%i", i);
-	if (i < 100)
-	printf(" ");
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%i", i);
+if (i < 100)
+printf(" ");
 }
 printf("\n");
 return (0);
