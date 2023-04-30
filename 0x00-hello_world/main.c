8db1f914f6cfe5b7ee953e4fd15b6e01d03fdd1f
yahonss <safahmimda@gmail.com>
2023-04-30 03:34:17 -0700

    new commit

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/0x00$ ./100-intel 
+julien@ubuntu:~/c/0x00$ cat main.s
+    .file   "main.c"
+    .intel_syntax noprefix
+    .text
+    .globl  main
+    .type   main, @function
+main:
+.LFB0:
+    .cfi_startproc
+    push    rbp
+    .cfi_def_cfa_offset 16
+    .cfi_offset 6, -16
+    mov rbp, rsp
+    .cfi_def_cfa_register 6
+    mov eax, 0
+    pop rbp
+    .cfi_def_cfa 7, 8
+    ret
+    .cfi_endproc
+.LFE0:
+    .size   main, .-main
+    .ident  "GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+    .section    .note.GNU-stack,"",@progbits
