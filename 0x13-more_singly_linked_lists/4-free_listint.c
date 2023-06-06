3653eeacfc7c4943970214efc1dd6e82e6958893
yahonss <safahmimda@gmail.com>
2023-06-06 13:28:24 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint - frees a linked list
+ * free_listint - function that frees a listint_t list
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
