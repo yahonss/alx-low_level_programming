d2a80e25c2b1861cb96cc64a18bf4385b3dde18c
yahonss <safahmimda@gmail.com>
2023-06-08 11:13:32 -0700

    new commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endiannes
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *) &i;
+
+	return (*c);
+}
+
