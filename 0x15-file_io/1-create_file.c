73183b6092cae4dbb387eb95853c782df514cf0d
yahonss <safahmimda@gmail.com>
2023-06-13 12:54:09 -0700

    new commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	fid = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
+	wr = write(fid, text_content, len);
 
 	if (fid == -1 || wr == -1)
 		return (-1);
