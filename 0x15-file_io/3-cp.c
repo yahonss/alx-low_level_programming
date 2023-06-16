c2b7205aa41a223d8f1cc507b2ce98bd959c1ad8
yahonss <safahmimda@gmail.com>
2023-06-16 13:07:34 -0700

    new commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void close_file(int fdval);
+char *create_buffer(char *file);
+
+/**
+ * create_buffer - Allocates 1,024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
+ */
+char *create_buffer(char *file)
+{
+        char *buffer;
+
+        buffer = malloc(sizeof(char) * 1024);
+
+        if (buffer == NULL)
+        {
+                dprintf(STDERR_FILENO,
+                        "Error: Can't write to %s\n", file);
+                exit(99);
+        }
+
+        return (buffer);
+}
+
+/**
+ * close_file - Closes the files
+ * @fdval: The file that will be closed.
+ */
+void close_file(int fdval)
+{
+        int cls;
+
+        cls = close(fdval);
+
+        if (cls == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fdval %d\n", fdval);
+                exit(100);
+        }
+}
+
