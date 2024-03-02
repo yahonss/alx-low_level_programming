e054e398865dffc99d75e1d041bc4e81830b16fb
yahonss <safahmimda@gmail.com>
2024-03-02 08:33:50 -0800

    ox14-BitManipulation

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,47 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+void close_file(int fdval);
+char *create_buffer(char *file);
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
+ * create_buffer - Allocates 1,024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-void error_file(int file_from, int file_to, char *argv[])
+char *create_buffer(char *file)
 {
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+        char *buffer;
+
+        buffer = malloc(sizeof(char) * 1024);
+
+        if (buffer == NULL)
+        {
+                dprintf(STDERR_FILENO,
+                        "Error: Can't write to %s\n", file);
+                exit(99);
+        }
+
+        return (buffer);
 }
 
 /**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * close_file - Closes the files
+ * @fdval: The file that will be closed.
  */
-int main(int argc, char *argv[])
+void close_file(int fdval)
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+        int cls;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
+        cls = close(fdval);
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	return (0);
+        if (cls == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fdval %d\n", fdval);
+                exit(100);
+        }
 }
+
+
